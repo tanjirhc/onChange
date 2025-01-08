d72b2cc2a2104b0c505c8431341135e128ffd3b1
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-01-09 00:11:26 +0600

    useState initial value

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -3,7 +3,7 @@ import React, {useState} from 'react';
 function MyComponent(){
 
 
-  const [name, setName] = useState("");
+  const [name, setName] = useState("Guest");
 
   function handleNameChange(event){
     setName(event.target.value);
