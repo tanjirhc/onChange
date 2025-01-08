a67a2b378e2d7458b8a2af1f841835030986ef88
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-01-09 00:25:35 +0600

    comment useState

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -5,6 +5,7 @@ function MyComponent(){
 
   const [name, setName] = useState("Guest");
   const [quantity, setQuantity] = useState(1);
+  const [comment, setComment] = useState("Comment");
 
   function handleNameChange(event){
     setName(event.target.value);
@@ -14,12 +15,21 @@ function MyComponent(){
     setQuantity(event.target.value)
   }
 
+  function handleCommentChange(event){
+    setComment(event.target.value);
+  }
+
   return(<div>
 
       <input value={name} onChange={handleNameChange}/>
       <p>Name: {name}</p>
+
       <input value={quantity} onChange={handleQuantityChange} type='number'/>
       <p>Quantity: {quantity}</p>
+
+      <textarea value={comment} onChange={handleCommentChange} placeholder='Enter your instructions'/>
+      <p>Comment: {comment}</p>
+
   </div>);
 }
 export default MyComponent;
