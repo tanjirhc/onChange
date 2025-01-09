f1b84060dce2abc6bacb08867856a7c0b0007b86
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-01-09 07:49:21 +0600

    radio button onChange

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -4,6 +4,7 @@ function MyComponent() {
   const [quantity, setQuantity] = useState(1);
   const [comment, setComment] = useState("Comment");
   const [payment, setPayment] = useState("Visa");
+  const [shipping, setShipping] = useState("");
 
   function handleNameChange(event) {
     setName(event.target.value);
@@ -19,6 +20,10 @@ function MyComponent() {
 
   function handlePaymentChange(event) {
     setPayment(event.target.value);
+  }
+
+  function handleShippingChange(event){
+    setShipping(event.target.value);
   }
 
   return (
@@ -43,6 +48,20 @@ function MyComponent() {
         <option value="Giftcard">Giftcard</option>
       </select>
       <p>Payment : {payment}</p>
+
+      <label>
+        <input type="radio" value="Pick Up"
+                checked={shipping === "Pick Up"}
+                onChange={handleShippingChange}/>
+        Pick Up
+      </label><br/>
+      <label>
+      <input type="radio" value="Delivery"
+                checked={shipping === "Delivery"}
+                onChange={handleShippingChange}/>
+        Delivery
+      </label>
+      <p>Shipping : {shipping}</p>
     </div>
   );
 }
