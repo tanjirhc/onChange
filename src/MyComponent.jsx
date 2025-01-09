fa02ff62d492bcde0f3d69e097a71918800b0a47
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-01-09 07:42:47 +0600

    payment useState

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -1,35 +1,49 @@
-
-import React, {useState} from 'react';
-function MyComponent(){
-
-
+import React, { useState } from "react";
+function MyComponent() {
   const [name, setName] = useState("Guest");
   const [quantity, setQuantity] = useState(1);
   const [comment, setComment] = useState("Comment");
+  const [payment, setPayment] = useState("Visa");
 
-  function handleNameChange(event){
+  function handleNameChange(event) {
     setName(event.target.value);
   }
 
-  function handleQuantityChange(event){
-    setQuantity(event.target.value)
+  function handleQuantityChange(event) {
+    setQuantity(event.target.value);
   }
 
-  function handleCommentChange(event){
+  function handleCommentChange(event) {
     setComment(event.target.value);
   }
 
-  return(<div>
+  function handlePaymentChange(event) {
+    setPayment(event.target.value);
+  }
 
-      <input value={name} onChange={handleNameChange}/>
+  return (
+    <div>
+      <input value={name} onChange={handleNameChange} />
       <p>Name: {name}</p>
 
-      <input value={quantity} onChange={handleQuantityChange} type='number'/>
+      <input value={quantity} onChange={handleQuantityChange} type="number" />
       <p>Quantity: {quantity}</p>
 
-      <textarea value={comment} onChange={handleCommentChange} placeholder='Enter your instructions'/>
+      <textarea
+        value={comment}
+        onChange={handleCommentChange}
+        placeholder="Enter your instructions"
+      />
       <p>Comment: {comment}</p>
 
-  </div>);
+      <select value={payment} onChange={handlePaymentChange}>
+        <option value="">Select an option</option>
+        <option value="Visa">Visa</option>
+        <option value="Mastercard">Mastercard</option>
+        <option value="Giftcard">Giftcard</option>
+      </select>
+      <p>Payment : {payment}</p>
+    </div>
+  );
 }
 export default MyComponent;
